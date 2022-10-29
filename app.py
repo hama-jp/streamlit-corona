217109084c40e070adda44e01c1e9fc52fdfea92
hama-jp <hama@mac.com>
2022-10-29 19:28:42 +0900

    3rd commit

--- app.py
+++ app.py
@@ -3,22 +3,25 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
-# 厚生労働省の公開データ
-url = 'https://covid19.mhlw.go.jp/public/opendata/newly_confirmed_cases_daily.csv'
-df = pd.read_csv(url, parse_dates=[0])
+try:
+    # 厚生労働省の公開データ
+    url = 'https://covid19.mhlw.go.jp/public/opendata/newly_confirmed_cases_daily.csv'
+    df = pd.read_csv(url, parse_dates=[0])
 
-st.sidebar.write('表示データを選択してください。')
-days = st.sidebar.slider('## 表示日数選択（過去日）', 1, 365*2, 365)
+    st.sidebar.write('表示データを選択してください。')
+    days = st.sidebar.slider('## 表示日数選択（過去日）', 1, 365*2, 365)
 
-st.title('一日あたりの陽性者数')
-pref=st.multiselect(
-    '表示データを選択してください。',
-    list(df.columns[1:]),
-    ['Tokushima', 'Kagawa', 'Ehime', 'Kochi']
-)
+    st.title('一日あたりの陽性者数')
+    pref=st.multiselect(
+        '表示データを選択してください。',
+        list(df.columns[1:]),
+        ['Tokushima', 'Kagawa', 'Ehime', 'Kochi']
+    )
 
-if not pref:
-    st.error('少なくとも一つは選んでください。')
-else:
-    fig = px.line(df[-days:], x='Date', y=pref, title="新型コロナ陽性者数の推移")
-    st.write(fig)
+    if not pref:
+        st.error('少なくとも一つは選んでください。')
+    else:
+        fig = px.line(df[-days:], x='Date', y=pref, title="新型コロナ陽性者数の推移")
+        st.write(fig)
+except:
+    st.error('おっと！何かエラーが起きているようです。')
